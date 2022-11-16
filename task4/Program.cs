e934b53b0ac66885d3c2f27216ea2521d395662d
Artem <temazasport@mail.ru>
2022-11-16 13:41:01 +0800

    S3_task4

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+System.Console.WriteLine("Введите число");
+int Num = int.Parse(Console.ReadLine());
+System.Console.WriteLine($"Квадраты чисел от 1 до {Num}");
+for (int i = 1; i <= Num; i++)
+{
+    
+    System.Console.Write($"{i*i}  ");
+}
+System.Console.WriteLine();
