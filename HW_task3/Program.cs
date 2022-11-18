5234f884a134ab88bc6ff021069b496e216c59d4
Artem Zabrodenko <temazasport@mail.ru>
2022-11-18 11:11:00 +0800

    hw_task3

---
+++ HW_task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+System.Console.WriteLine("Введите число");
+int Num = int.Parse(Console.ReadLine());
+System.Console.WriteLine($"Кубы чисел от 1 до {Num}");
+for (int i = 1; i < Num; i++)
+{
+    
+    System.Console.Write($"{Math.Pow(i,3)}, ");
+}
+
+System.Console.WriteLine(Math.Pow(Num,3));
