fd57a5c336907c5cbf3b4d1ebbc5d51deca14f3d
Artem Zabrodenko <temazasport@mail.ru>
2022-11-18 10:48:48 +0800

    hw_task2_1

--- HW_task2/Program.cs
+++ HW_task2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿   //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+        Console.WriteLine("Введите Х1");
+        double NumX1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите Y1");
+        double NumY1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите Z1");
+        double NumZ1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите Х2");
+        double NumX2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите Y2");
+        double NumY2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите Z3");
+        double NumZ2 = double.Parse(Console.ReadLine());
+
+        double distantion = Math.Sqrt(Math.Pow(NumX2 - NumX1, 2) + Math.Pow(NumY2 - NumY1, 2) + Math.Pow(NumZ2 - NumZ1, 2));
+        Console.WriteLine($"расстояние между точками = {distantion}");
+    
